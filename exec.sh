64317f894ccb2449fedd492c158c6fc9441b357f
guo <397388093@qq.com>
2016-04-21 16:32:47 +0800

    exec

---
+++ exec.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -cp ./jmetal-exec/target/jmetal-exec-5.1-SNAPSHOT.jar:./jmetal-algorithm/target/jmetal-algorithm-5.1-SNAPSHOT.jar:./jmetal-core/target/jmetal-core-5.1-SNAPSHOT.jar:./jmetal-problem/target/jmetal-problem-5.1-SNAPSHOT.jar org.uma.jmetal.runner.singleobjective.WindFLOCMESRunner
